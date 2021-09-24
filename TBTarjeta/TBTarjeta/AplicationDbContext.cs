4affa5a2e67ae9d333147096033725de1d41261a
JhonM <Mjhon1841@gmail.com>
2021-09-23 22:58:39 -0500

    Se añade carpeta con el backEnd

---
+++ TBTarjeta/TBTarjeta/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBTarjeta.Models;
+
+namespace TBTarjeta
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { set; get; }
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+
+    }
+}
